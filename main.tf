df3ceffcfd026d67bead2b474acd7c7bc938be9b
PriyankaRavee <priyankacsengr@gmail.com>
2025-10-06 00:34:03 +0530

    fixing bug

--- main.tf
+++ main.tf
@@ -8,51 +8,22 @@ resource "aws_key_pair" "devops_key" {
   public_key = file("${path.module}/devops-key.pub")
 }
 
-resource "aws_security_group" "devops_sg" {
-  name        = "devops-sg"
-  description = "Allow SSH, HTTP, Backend, Frontend"
+# resource "aws_security_group" "devops_sg" {
+#   name        = "devops-sg"
+#   description = "Allow SSH, HTTP, Backend, Frontend"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "devops_sg" {
+  name = "devops-sg"
 }
 
 resource "aws_instance" "devops_vm" {
   ami           = "ami-02d26659fd82cf299" # Ubuntu 20.04 in ap-south-1
   instance_type = var.instance_type
   key_name      = aws_key_pair.devops_key.key_name
-  security_groups = [aws_security_group.devops_sg.name]
+  # security_groups = [aws_security_group.devops_sg.name]
+  vpc_security_group_ids = [data.aws_security_group.devops_sg.id]
+
   # instance_state = "stopped"
 
   tags = {
